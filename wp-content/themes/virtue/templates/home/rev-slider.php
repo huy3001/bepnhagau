76856f10d2d374b4b2ff785ff92bbdd2a9b4d192
Nguyen Cong Huy <nguyenconghuy@sutunam.com>
2017-04-24 17:57:43 +0700

    Add wordpress theme

---
+++ wp-content/themes/virtue/templates/home/rev-slider.php
@@ -0,0 +1,16 @@
+<?php
+	global $virtue_premium; 
+
+?>
+<div class="sliderclass home_sliderclass revslider_home_hidetop">
+<?php  
+	if( function_exists('putRevSlider') ) {
+		putRevSlider( $virtue_premium['rev_slider'] );
+		if(isset($virtue_premium['above_header_slider_arrow']) && $virtue_premium['above_header_slider_arrow'] == 1) {
+        	echo '<div class="kad_fullslider_arrow"><a href="#home_slider_bottom"><i class="icon-arrow-down"></i></a></div>';
+      	}
+	} else {
+		echo '<p class="error" style="text-align:center; color: red;">'.__("Please Install Revolution Slider Plugin", "virtue").'</p>';
+	} ?>
+</div><!--sliderclass-->
+<div id="home_slider_bottom"></div>
